0546d0794ad219593516c7bcffbc9853855e3aba
Morten Aggerholm <morten@mortenaggerholm.dk>
2018-08-29 21:12:47 +0530

    Updated db.sql - Create Table - Defined more fields

--- db.sql
+++ db.sql
@@ -5,6 +5,13 @@ CREATE TABLE Ejerlejlighed (
   Body TEXT NOT NULL,
   Lejligheden TEXT,
   Ejendommen TEXT,
+  Boligareal VARCHAR (100) NOT NULL,
+  Rum VARCHAR (100),
+  Year VARCHAR (100),
+  Energimaerke VARCHAR (100),
+  Pris VARCHAR (100) NOT NULL,
+  PrisPrm2 VARCHAR (100),
+  Ejerudgifter VARCHAR (100) NOT NULL,
   Status VARCHAR (100),
   Posteddata DATE
 ) ENGINE = InnoDB;
