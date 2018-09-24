358e1c902dd77af1261293653492bbb47ab1dfcd
Morten Aggerholm <morten@mortenaggerholm.dk>
2018-09-24 21:04:04 +0200

    Changed images.html to images.php and added titel

---
+++ images.php
@@ -0,0 +1,35 @@
+<!-- Added index.html header -->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+   <html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="description" content="Ejerlejlighed Sælges NU!">
+        <meta name="keywords" content="Ejerlejlighed, Vallensbæk Starnd, Ejerlejlighed Sælges">
+        <meta name="author" content="Morten Aggerholm">
+        <title>Ejerlejlighed Sælges NU!</title>
+        <link href="./styles/styles.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+      <div id="container"> <!--start .container-->
+        <header id="mainHeader">
+          <a href="index.html>"><img src="design/top.png" alt="Top Image"></a>
+          <div id="path"> <!--start .path-->
+              <ul>
+                <li>Du er her: </li>
+                <li><a href="index.php">Forside</a>&gt;</li>
+                <li><a href="images.html">Billeder</a></li>
+              </ul>
+          </div> <!--end .path--> 
+        </header> <!-- end .header-->
+        <section id="houseInfo">
+          <h1><?php echo $titel; ?></h1>
+        </section>
+        <footer id="pageFooter"> <!--start .pageFooter-->
+          <p>Kontaktoplysninger: Morten Aggerholm - Tlf. 29 70 41 32 - <a href="mailto:morten@mortenaggerholm.dk">E-mail: morten(at)mortenaggerholm.dk</a></p>
+          <p>&copy Copyright 2018 - Alle Rettigheder Forbeholdes</p>
+        </footer> <!--end.container--> 
+      </div><!--end.container--> 
+
+    </body>
+  </html>
