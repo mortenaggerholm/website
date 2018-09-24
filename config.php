636e19e8187f94810b7bed828f257184eae58f34
Morten Aggerholm <morten@mortenaggerholm.dk>
2018-09-24 21:12:09 +0200

    Added config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+int_set("display_errors", 0);
+
+$dir = 'images/largeImages'; // Sti til folderen med billeder
+$maxwidth = '150'; // in Pixels
+$maxheight = '100'; // in Pixels
+$titel = 'Billeder fra lejligheden'; // Titel på billedgalleri
+$subtitle = 'Vejlegårdsparken 48,1,18 - 2665 Vallensbæk Strand'; // Undertitel på billedgalleri
+?>
+
+
+
