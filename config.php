d788e1c00e8651274466a1d22d8d3b6db597f472
Morten Aggerholm <morten@mortenaggerholm.dk>
2018-09-27 21:14:37 +0200

    Added PHP code to load images from directory

--- config.php
+++ config.php
@@ -6,6 +6,19 @@ $maxwidth = '150'; // in Pixels
 $maxheight = '100'; // in Pixels
 $title = 'Billeder fra lejligheden'; // Titel på billedgalleri
 $subtitle = 'Vejlegårdsparken 48,1,18 - 2665 Vallensbæk Strand'; // Undertitel på billedgalleri
+
+$dirHandle = opendir($dir);
+$count = -1;
+$returnstr = "";
+while ($file = readdir($dirHandle)) {
+    if(!is_dir($file) && strpos($file, '.jpg')>0) {
+        $count++;
+        $image[$count] = $file;
+    }
+}
+$column=0;
+$maxcolumns = $maxcolumns-2;
+closedir($dirHandle);
 ?>
 
 
